9fc5136e44d6a4f466d21d9007d2b3a1c15dd48e
KoSeongHoon <skypink7123@g.shingu.ac.kr>
2026-03-22 13:50:57 +0900

    a와 b 출력하기 ,문자열 반복 출력하기 코테

---
+++ Coding Test/Test5/Test5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/181951
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}

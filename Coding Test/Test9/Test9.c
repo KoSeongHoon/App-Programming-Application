583abcc432b18cd1d268572e19dc6e08c1bff35f
KoSeongHoon <skypink7123@g.shingu.ac.kr>
2026-03-27 20:13:58 +0900

    덧셈 출력,문자열 붙여서 출력 코테

---
+++ Coding Test/Test9/Test9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/181947
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d", a, b, a + b);
+    return 0;
+}
